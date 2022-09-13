6e257f1027f994d9d6781e849b1525d7eca9012f
muhiqbalm <muhammadiqbal2020@mail.ugm.ac.id>
2022-09-13 10:09:24 +0700

    add class user

---
+++ Petanee/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petanee
+{
+    class User
+    {
+        public int userID;
+        public string username { get; set; }
+        public string password { get; set; }
+        public string userLocation;
+        public string locationClimate;
+
+        public Boolean login(string username, string password)
+        {
+            return true;
+        }
+    }
+}
