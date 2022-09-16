87c84212c76d82fcae7b6157b689620fc1971ec8
sonia <79226247+ipungsonia@users.noreply.github.com>
2022-09-16 14:52:22 +0700

    add class condition

---
+++ Petanee/Condition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petanee
+{
+    class Condition
+    {
+        public string locClimate;
+        public string plantClimate;
+        public string plantCategory;
+        public int plantID;
+
+        public int getPlanID(string locClimate, string plantClimate, string plantCategory)
+        {
+            return plantID;
+        }
+    }
+}
