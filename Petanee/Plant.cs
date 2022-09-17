cef52cad2b805fdd6551437b8589a5eaa7f49f72
anantapridya <ananta20pridya@gmail.com>
2022-09-17 19:18:09 +0700

    push class plant

---
+++ Petanee/Plant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petanee
+{
+    class Plant
+    {
+        public int plantID;
+        public string plantName;
+        public string plantCategory;
+        public string plantClimate;
+        public string harvestTime;
+        public string wateringTime;
+
+        public void showDescription(int plantID, string plantName, string plantCategory, string plantClimate, string harvestTime, string wateringTime)
+        {
+
+        }
+    }
+}
